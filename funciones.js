c9dd94e3b9d3d0951259756f1b511757e08aabf8
jprera <55000865+jprera@users.noreply.github.com>
2022-04-20 00:21:18 -0600

    video9 curso gas-avanzado

--- funciones.js
+++ funciones.js
@@ -5,3 +5,10 @@ function doGet() {
 function obtenerDatosHTML(nombre) {
     return HtmlService.createHtmlOutputFromFile(nombre).getContent();
 }
+
+function obtenerContactos() {
+    let hoja = SpreadsheetApp.openById('1C35y55O8feQZdgQ9UpCPU_2tngA3TPIRcettEm2pSI0').getActiveSheet();
+    let datos = hoja.getDataRange().getValues();
+    return datos;
+
+}
