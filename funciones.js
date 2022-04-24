478b599ba749a8577cb8f99065c080df2100ab72
jprera <55000865+jprera@users.noreply.github.com>
2022-04-24 15:29:15 -0600

    video12 curso gas-avanzado

--- funciones.js
+++ funciones.js
@@ -10,5 +10,6 @@ function obtenerContactos() {
     let hoja = SpreadsheetApp.openById('1C35y55O8feQZdgQ9UpCPU_2tngA3TPIRcettEm2pSI0').getActiveSheet();
     let datos = hoja.getDataRange().getValues();
     return datos;
+    
 
 }
