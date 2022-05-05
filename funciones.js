c172ec4f462389e157fa22ed0e384b42d1823fb0
jprera <55000865+jprera@users.noreply.github.com>
2022-05-05 01:12:44 -0600

    video 15 curso gas-avnazado

--- funciones.js
+++ funciones.js
@@ -1,15 +1,23 @@
-function doGet() {
-    return HtmlService.createTemplateFromFile('web').evaluate().setTitle('Agenda');
-}
+    const HOJA = SpreadsheetApp.openById('1C35y55O8feQZdgQ9UpCPU_2tngA3TPIRcettEm2pSI0').getActiveSheet();
 
-function obtenerDatosHTML(nombre) {
-    return HtmlService.createHtmlOutputFromFile(nombre).getContent();
-}
+    function doGet(datos) {
+        return HtmlService.createTemplateFromFile('web').evaluate().setTitle('Agenda');
+    }
 
-function obtenerContactos() {
-    let hoja = SpreadsheetApp.openById('1C35y55O8feQZdgQ9UpCPU_2tngA3TPIRcettEm2pSI0').getActiveSheet();
-    let datos = hoja.getDataRange().getValues();
-    return datos;
-    
+    function doPost(datos) {
+        return HtmlService.createTemplateFromFile('web').evaluate().setTitle('Agenda');
 
-}
+    }
+
+    function obtenerDatosHTML(nombre) {
+        return HtmlService.createHtmlOutputFromFile(nombre).getContent();
+    }
+
+    function obtenerContactos() {
+        return HOJA.getDataRange().getValues();
+    }
+
+    function insertarContacto(nombre, correo) {
+
+        HOJA.appendRow([nombre, correo]);
+    }
