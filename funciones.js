0536f2a2e63f36cd0a049af53d9b807e2ae2c991
jprera <55000865+jprera@users.noreply.github.com>
2022-06-05 00:36:59 -0600

    video26 curso-gas-avanzado

--- funciones.js
+++ funciones.js
@@ -1,46 +1,45 @@
-    const HOJA = SpreadsheetApp.openById('1C35y55O8feQZdgQ9UpCPU_2tngA3TPIRcettEm2pSI0').getActiveSheet();
+const HOJA = SpreadsheetApp.openById('1C35y55O8feQZdgQ9UpCPU_2tngA3TPIRcettEm2pSI0').getActiveSheet();
 
-    function doGet(datos) {
-        return HtmlService.createTemplateFromFile('web').evaluate().setTitle('Agenda');
-    }
+function doGet(datos) {
+    return HtmlService.createTemplateFromFile('web').evaluate().setTitle('Agenda');
+}
 
-    function doPost(datos) {
-        return HtmlService.createTemplateFromFile('web').evaluate().setTitle('Agenda');
+function doPost(datos) {
+    return HtmlService.createTemplateFromFile('web').evaluate().setTitle('Agenda');
 
-    }
+}
 
-    function obtenerDatosHTML(nombre) {
-        return HtmlService.createHtmlOutputFromFile(nombre).getContent();
-    }
+function obtenerDatosHTML(nombre) {
+    return HtmlService.createHtmlOutputFromFile(nombre).getContent();
+}
 
-    function obtenerContactos() {
-        return HOJA.getDataRange().getValues();
-    }
+function obtenerDatos() {
+    return HOJA.getDataRange().getValues();
+}
 
-    function insertarContacto(nombre, apellidos, correo, telf) {
+function insertarContacto(nombre, apellidos, correo, telf) {
 
-        HOJA.appendRow([nombre, apellidos, correo, telf]);
-    }
+    HOJA.appendRow([nombre, apellidos, correo, telf]);
+}
 
-    function borrarContacto(numFila) {
-        HOJA.deleteRow(numFila);
-    }
+function borrarContacto(numFila) {
+    HOJA.deleteRow(numFila);
+}
 
-    function modificarContacto(numFila, datos) {
-        let celdas = HOJA.getRange('A' + numFila + ':D' + numFila);
-        celdas.setValues([
-            [datos.nombre, datos.apellidos, datos.correo, datos.telf]
-        ]);
-    }
+function modificarContacto(numFila, datos) {
+    let celdas = HOJA.getRange('A' + numFila + ':D' + numFila);
+    celdas.setValues([
+        [datos.nombre, datos.apellidos, datos.correo, datos.telf]
+    ]);
+}
 
-    function importarContactos() {
-        let url ='https://randomuser.me/api/?results=5&inc=name,email,phone,picture';
-        let respuesta = UrlFetchApp.fetch(url).getContentText();
-        let datos = JSON.parse(respuesta);  
+function importarContactos() {
+    let url = 'https://randomuser.me/api/?results=5&inc=name,email,phone,picture';
+    let respuesta = UrlFetchApp.fetch(url).getContentText();
+    let datos = JSON.parse(respuesta);
 
-        datos.results.forEach(insertarContactoJSON);        
-    }
-    function insertarContactoJSON(contacto){
-    HOJA.appendRow([contacto.name.first,contacto.name.last,contacto.email,contacto.phone, contacto.picture.large]);
-    }
-
+    datos.results.forEach(insertarContactoJSON);
+}
+function insertarContactoJSON(contacto) {
+    HOJA.appendRow([contacto.name.first, contacto.name.last, contacto.email, contacto.phone, contacto.picture.large]);
+}
